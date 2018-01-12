1e0e27f6ab6345cedb44e7d870631afc865d9082
SherMarri <marribaloch128@gmail.com>
2018-01-12 15:24:16 +0500

    SoftDeletionModel implemented

--- IMSAdmin/models.py
+++ IMSAdmin/models.py
@@ -1,3 +1,56 @@
-from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from django.db import models
+from django.db.models import QuerySet
+
+'''
+The Soft Delete functionaly code has bee copied from : https://medium.com/@adriennedomingus/soft-deletion-in-django-e4882581c340
+It implements the Soft Delete, which means, it doesn't actually delete the object in real, just marks it as deleted
+'''
+
+class SoftDeletionQuerySet(QuerySet):
+    def delete(self):
+        return super(SoftDeletionQuerySet, self).update(deleted_at=timezone.now())
+
+    def hard_delete(self):
+        return super(SoftDeletionQuerySet, self).delete()
+
+    def alive(self):
+        return self.filter(deleted_at=None)
+
+    def dead(self):
+        return self.exclude(deleted_at=None)
+
+class SoftDeletionManager(models.Manager):
+    def __init__(self, *args, **kwargs):
+        self.alive_only = kwargs.pop('alive_only', True)
+        super(SoftDeletionManager, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        if self.alive_only:
+            return SoftDeletionQuerySet(self.model).filter(deleted_at=None)
+        return SoftDeletionQuerySet(self.model)
+
+    def hard_delete(self):
+        return self.get_queryset().hard_delete()
+
+class SoftDeletionModel(models.Model):
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    objects = SoftDeletionManager()
+    all_objects = SoftDeletionManager(alive_only=False)
+
+    class Meta:
+        abstract = True
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def hard_delete(self):
+        super(SoftDeletionModel, self).delete()
+
+
+class AdminProfile(SoftDeletionModel):
+    contact = models.CharField(max_length=20)
+    city = models.CharField(max_length=20, null= True)
