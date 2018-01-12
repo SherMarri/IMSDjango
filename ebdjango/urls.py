20e2dda2f132ee9a5110202e45bf2bd3f63c07ce
SherMarri <marribaloch128@gmail.com>
2018-01-12 12:33:28 +0500

    Admin Site Added and Django Version Issue Resolved

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-   url(r'^admin/', admin.site.urls),
+   url('admin/', admin.site.urls),
    url(r'^', include('helloworld.urls')), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
